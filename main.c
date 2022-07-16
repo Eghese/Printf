1d6f70274ff857a06a322515accab1722a8eeb14
Eghese <eghese123@gmail.com>
2022-07-15 20:21:34 -0700

    print_address.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _printf - write a function that produces output of format.
+ * @format: a char format
+ * Return: success
+ */
+int _printf(const char *format, ...)
+{
+
---
+++ print_decimal.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * print_decimal - prints a digit on the stdout
+ * @value: Parameter passed
+ * Return: count
+ */
+int print_decimal(int value)
+{
+	int count = o;
+
+	if (value >= 0)
+	{
+		
+	}
+	return (count);
+}
